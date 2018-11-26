60e82d59ad407ccdada7031095272e6deb1cca50
MohabMohamed <mohab.m.mohamed@gmail.com>
2018-11-26 13:14:55 +0200

    fix the VS project on windows

--- BtatesCraft/src/VertexBufferLayout.h
+++ BtatesCraft/src/VertexBufferLayout.h
@@ -2,7 +2,7 @@
 #include <vector>
 #include <GL/glew.h>
 #include <GLErrorHandler.h>
-
+#include <assert.h>
 
 struct VertexBufferElement
 {
@@ -32,6 +32,11 @@ class VertexBufferLayout
 private:
 	std::vector<VertexBufferElement> m_Elements;
 	unsigned int m_stride;
+	template <typename... T>
+	struct wrong_param
+	{
+		static constexpr bool value = false;
+	};
 public:
 	VertexBufferLayout():m_stride(0)
 	{
@@ -39,11 +44,41 @@ public:
 	}
 	~VertexBufferLayout() {}
 
-	template <typename T>
-	void Push(unsigned count) {
-        static_assert("Push needs to be overridden for the typename T provided");
-	}
-
 	inline unsigned int GetStride() const { return m_stride; }
 	inline const std::vector<VertexBufferElement>& GetElements() const { return m_Elements; } //should add &
+	
+	template <typename T>
+	void Push(unsigned count) {
+		static_assert(wrong_param<T>::value, "Push needs to be overridden for the typename T provided");
+	}
+
+	template <>
+	void Push<float>(unsigned int count)
+	{
+		m_Elements.push_back({ GL_FLOAT,count,false });
+		m_stride += count * VertexBufferElement::GetTypeSize(GL_FLOAT);
+	}
+
+	template <>
+	void Push<unsigned int>(unsigned int count)
+	{
+		m_Elements.push_back({ GL_UNSIGNED_INT,count,false });
+		m_stride += count *  VertexBufferElement::GetTypeSize(GL_UNSIGNED_INT);
+	}
+	template <>
+	void Push<int>(unsigned int count)
+	{
+		m_Elements.push_back({ GL_INT,count,false });
+		m_stride += count *  VertexBufferElement::GetTypeSize(GL_INT);
+	}
+
+	template <>
+	void Push<unsigned char>(unsigned int count)
+	{
+		m_Elements.push_back({ GL_UNSIGNED_BYTE,count,true });
+		m_stride += VertexBufferElement::GetTypeSize(GL_UNSIGNED_BYTE);
+	}
+
+
+
 };
